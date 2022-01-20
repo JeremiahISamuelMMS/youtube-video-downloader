86d514e25767832b5c41347a96dd6652d44ed354
Jeremiah Ihab Samuel <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2022-01-20 00:47:23 +0000

    1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# YouTube Video Downloader
+  # YouTube Video Downloader
 Build with Sevelte
 <img height="180px" width="220px" src="https://raw.githubusercontent.com/sveltejs/svelte/29052aba7d0b78316d3a52aef1d7ddd54fe6ca84/site/static/images/svelte-android-chrome-512.png" alt="Build Tech">
 
--- src/components/views/ResultPanel.svelte
+++ src/components/views/ResultPanel.svelte
@@ -6,9 +6,9 @@
   <span class="title"> List of download formats</span>
 
   <div class="buttons">
-    <iframe class="" src="https://api.download-lagu-mp3.com/@api/button/mp3/{video_id}" width="33%" height="100%" allowtransparency="true" scrolling="no" style="border:none"></iframe>
-    <iframe class="" src="https://api.download-lagu-mp3.com/@api/button/videostreams/{video_id}" width="33%" height="100%" allowtransparency="true" scrolling="no" style="border:none"></iframe>
-    <iframe class="" src="https://api.download-lagu-mp3.com/@api/button/mergedstreams/{video_id}" width="33%" height="100%" allowtransparency="true" scrolling="no" style="border:none"></iframe>
+    <iframe class="" src="https://api.download-lagu-mp3.com/@api/button/mp3/{video_id}" width="33%" height="100%" allowtransparency="true" scrolling="no" style="border:none" title=""></iframe>
+    <iframe title="" class="" src="https://api.download-lagu-mp3.com/@api/button/videostreams/{video_id}" width="33%" height="100%" allowtransparency="true" scrolling="no" style="border:none"></iframe>
+    <iframe title="" class="" src="https://api.download-lagu-mp3.com/@api/button/mergedstreams/{video_id}" width="33%" height="100%" allowtransparency="true" scrolling="no" style="border:none"></iframe>
   </div>
 
 </div>
